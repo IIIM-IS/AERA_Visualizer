4c0816cb9239ee292698a6591f7ae5363a12a965
Jeff Thompson <jeff@thefirst.org>
2020-04-30 09:01:17 +0200

    Use doxygen doc comments to make Travis happy.

--- replicode-objects.hpp
+++ replicode-objects.hpp
@@ -5,29 +5,29 @@
 #include <map>
 #include "submodules/replicode/r_exec/mem.h"
 
-/// <summary>
-/// ReplicodeObjects holds a list of Replicode objects that are compiled from the
-/// decompiler output.
-/// </summary>
+/**
+ * ReplicodeObjects holds a list of Replicode objects that are compiled from the
+ * decompiler output.
+ */
 class ReplicodeObjects {
 public:
-  /// <summary>
-  /// Compile and load the metadata from the user operators file, then compile
-  /// the decompiled file and set up the list of Replicode objects. This
-  /// processes the decompile file to get the time reference and original OIDs.
-  /// </summary>
-  /// <param name="userOperatorsFilePath">The user operators file path, usually ending
-  /// in "user.classes.replicode".</param>
-  /// <param name="decompiledFilePath">The decompiled output, usually ending in
-  /// "decompiled_objects.txt".</param>
-  /// <returns>True for success, false for error compiling.</returns>
+  /**
+   * Compile and load the metadata from the user operators file, then compile
+   * the decompiled file and set up the list of Replicode objects. This
+   * processes the decompile file to get the time reference and original OIDs.
+   * \param userOperatorsFilePath The user operators file path, usually ending
+   * in "user.classes.replicode".
+   * \param decompiledFilePath The decompiled output, usually ending in
+   * "decompiled_objects.txt".
+   * \return True for success, false for error compiling.
+   */
   bool init(const std::string& userOperatorsFilePath, const std::string& decompiledFilePath);
 
-  /// <summary>
-  /// Get the time reference that was loaded from the decompiled output. 
-  /// This can be used to show times relative to the beginning of the Replicode session.
-  /// </summary>
-  /// <returns>The time reference.</returns>
+  /**
+   * Get the time reference that was loaded from the decompiled output. 
+   * This can be used to show times relative to the beginning of the Replicode session.
+   * \return The time reference.
+   */
   core::Timestamp getTimeReference() const { return timeReference_; }
 
 private:
