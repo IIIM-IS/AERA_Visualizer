d179f6e7c9c400ced85825a6d069a07bdefbf82f
Jeff Thompson <jeff@thefirst.org>
2020-05-14 12:13:51 +0200

    ExplanationLogWindow: In textBrowserAnchorClicked for #debug_oid, handle mk.rdx specially.

--- explanation-log-window.cpp
+++ explanation-log-window.cpp
@@ -1,8 +1,11 @@
 #include <QtWidgets>
+#include "submodules/replicode/r_exec/opcodes.h"
+#include "graphics-items/program-reduction-item.hpp"
 #include "graphics-items/aera-visualizer-scene.hpp"
 #include "explanation-log-window.hpp"
 
 using namespace std;
+using namespace r_exec;
 
 namespace aera_visualizer {
 
@@ -29,16 +32,39 @@ void ExplanationLogWindow::textBrowserAnchorClicked(const QUrl& url)
   if (url.url().startsWith("#debug_oid-")) {
     uint64 debug_oid = url.url().mid(11).toULongLong();
     auto object = replicodeObjects_.getObjectByDebugOid(debug_oid);
-    if (object) {
+    if (!object)
+      return;
+
+    if (object->code(0).asOpcode() == Opcodes::MkRdx) {
+      // There is no item for an mk.rdx, so show a menu for a "What Is" explanation.
+      auto menu = new QMenu();
+      menu->addAction("What Is This?", [=]() {
+        // TODO: Handle this in a static method of ProgramReductionItem.
+        QString reductionHtml = AeraGraphicsItem::htmlify(ProgramReductionItem::simplifyMkRdxSource(
+          replicodeObjects_.getSourceCode(object)));
+        AeraGraphicsItem::addSourceCodeHtmlLinks(object, reductionHtml, replicodeObjects_);
+
+        string explanation = "<u>Q: What is program reduction " + replicodeObjects_.getLabel(object) +
+          "?</u><br>This the notification of a reduction of instantiated program <b>" + 
+          replicodeObjects_.getLabel(object->get_reference(0)) + 
+          "</b><br>It has the following sets of input objects and output actions.<br>" + 
+          reductionHtml.toStdString() + "<br><br>";
+        appendHtml(explanation);
+      });
+      menu->exec(textBrowser_->mouseScreenPosition_ - QPoint(10, 10));
+      delete menu;
+    }
+    else {
       auto item = parent_->getScene()->getAeraGraphicsItem(object);
-      if (item) {
-        // Show the menu.
-        auto menu = new QMenu();
-        menu->addAction(QString("Zoom to ") + replicodeObjects_.getLabel(object).c_str(),
-          [=]() { parent_->getScene()->zoomToItem(item); });
-        menu->exec(textBrowser_->mouseScreenPosition_ - QPoint(10, 10));
-        delete menu;
-      }
+      if (!item)
+        return;
+
+      // Show the menu.
+      auto menu = new QMenu();
+      menu->addAction(QString("Zoom to ") + replicodeObjects_.getLabel(object).c_str(),
+        [=]() { parent_->getScene()->zoomToItem(item); });
+      menu->exec(textBrowser_->mouseScreenPosition_ - QPoint(10, 10));
+      delete menu;
     }
   }
 }
