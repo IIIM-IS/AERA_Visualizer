1754def6b5c809deaa923769c025685b8238af47
Jeff Thompson <jeff@thefirst.org>
2021-02-16 14:18:06 +0100

    AeraVisualizerScene: Make the scene higher.

--- graphics-items/aera-visualizer-scene.cpp
+++ graphics-items/aera-visualizer-scene.cpp
@@ -90,7 +90,7 @@ AeraVisualizerScene::AeraVisualizerScene(
   lineColor_ = Qt::black;
   setBackgroundBrush(QColor(245, 245, 245));
   flashTimerId_ = 0;
-  setSceneRect(QRectF(0, 0, 20000, 5000));
+  setSceneRect(QRectF(0, 0, 20000, 10000));
 
   if (isMainScene_) {
     eventTypeFirstTop_[IoDeviceEjectEvent::EVENT_TYPE] = 20;
