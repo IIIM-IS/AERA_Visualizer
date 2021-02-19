42b64dfbb7a653a9a41b80d5b757d0e6348a8822
Jeff Thompson <jeff@thefirst.org>
2021-02-19 17:32:17 +0100

    AnchoredHorizontalLine: Set DefaultPen to a dashed line.

--- graphics-items/anchored-horizontal-line.cpp
+++ graphics-items/anchored-horizontal-line.cpp
@@ -57,7 +57,7 @@
 
 namespace aera_visualizer {
 
-const QPen AnchoredHorizontalLine::DefaultPen(Qt::gray, 1, Qt::SolidLine, Qt::SquareCap);
+const QPen AnchoredHorizontalLine::DefaultPen(Qt::gray, 1, Qt::DashLine, Qt::SquareCap);
 const QPen AnchoredHorizontalLine::HighlightedPen(QColor(240, 240, 0), 3, Qt::SolidLine, Qt::SquareCap);
 
 AnchoredHorizontalLine::AnchoredHorizontalLine(
