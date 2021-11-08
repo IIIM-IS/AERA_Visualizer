0feabbdb7db14f8405e8696a5428c9f782052f86
jefft0 <jeff@thefirst.org>
2021-11-08 09:59:51 +0100

    global: Update copyright header for release.

--- graphics-items/auto-focus-fact-item.cpp
+++ graphics-items/auto-focus-fact-item.cpp
@@ -1,53 +1,56 @@
-//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 //_/_/
-//_/_/ AERA VISUALIZER
+//_/_/ AERA Visualizer
+//_/_/ 
+//_/_/ Copyright (c) 2021 Jeff Thompson
+//_/_/ Copyright (c) 2021 Kristinn R. Thorisson
+//_/_/ Copyright (c) 2021 Karl Asgeir Geirsson
+//_/_/ Copyright (c) 2021 Icelandic Institute for Intelligent Machines
+//_/_/ http://www.iiim.is
 //_/_/
-//_/_/ Copyright(c)2020 Icelandic Institute for Intelligent Machines ses
-//_/_/ Vitvelastofnun Islands ses, kt. 571209-0390
-//_/_/ Author: Jeffrey Thompson <jeff@iiim.is>
+//_/_/ --- Open-Source BSD License, with CADIA Clause v 1.0 ---
 //_/_/
-//_/_/ -----------------------------------------------------------------------
-//_/_/ Released under Open-Source BSD License with CADIA Clause v 1.0
-//_/_/
-//_/_/ Redistribution and use in source and binary forms, with or without 
-//_/_/ modification, is permitted provided that the following conditions 
+//_/_/ Redistribution and use in source and binary forms, with or without
+//_/_/ modification, is permitted provided that the following conditions
 //_/_/ are met:
-//_/_/
-//_/_/ - Redistributions of source code must retain the above copyright 
-//_/_/   and collaboration notice, this list of conditions and the 
+//_/_/ - Redistributions of source code must retain the above copyright
+//_/_/   and collaboration notice, this list of conditions and the
 //_/_/   following disclaimer.
+//_/_/ - Redistributions in binary form must reproduce the above copyright
+//_/_/   notice, this list of conditions and the following disclaimer 
+//_/_/   in the documentation and/or other materials provided with 
+//_/_/   the distribution.
 //_/_/
-//_/_/ - Redistributions in binary form must reproduce the above copyright 
-//_/_/   notice, this list of conditions and the following
-//_/_/   disclaimer in the documentation and/or other materials provided 
-//_/_/   with the distribution.
+//_/_/ - Neither the name of its copyright holders nor the names of its
+//_/_/   contributors may be used to endorse or promote products
+//_/_/   derived from this software without specific prior 
+//_/_/   written permission.
+//_/_/   
+//_/_/ - CADIA Clause: The license granted in and to the software 
+//_/_/   under this agreement is a limited-use license. 
+//_/_/   The software may not be used in furtherance of:
+//_/_/    (i)   intentionally causing bodily injury or severe emotional 
+//_/_/          distress to any person;
+//_/_/    (ii)  invading the personal privacy or violating the human 
+//_/_/          rights of any person; or
+//_/_/    (iii) committing or preparing for any act of war.
 //_/_/
-//_/_/ - Neither the name of its copyright holders nor the names of its 
-//_/_/   contributors may be used to endorse or promote products 
-//_/_/   derived from this software without specific prior written permission.
-//_/_/
-//_/_/ - CADIA Clause v 1.0: The license granted in and to the software under 
-//_/_/   this agreement is a limited-use license. The software may not be used
-//_/_/   in furtherance of: 
-//_/_/   (i) intentionally causing bodily injury or severe emotional distress 
-//_/_/   to any person; 
-//_/_/   (ii) invading the personal privacy or violating the human rights of 
-//_/_/   any person; or 
-//_/_/   (iii) committing or preparing for any act of war.
-//_/_/
-//_/_/ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-//_/_/ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-//_/_/ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
-//_/_/ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-//_/_/ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-//_/_/ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-//_/_/ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-//_/_/ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
-//_/_/ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-//_/_/ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-//_/_/ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//_/_/
-//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+//_/_/ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+//_/_/ CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+//_/_/ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+//_/_/ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+//_/_/ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+//_/_/ CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+//_/_/ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+//_/_/ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+//_/_/ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+//_/_/ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+//_/_/ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+//_/_/ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+//_/_/ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+//_/_/ OF SUCH DAMAGE.
+//_/_/ 
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 
 #include <regex>
 #include <QMenu>
