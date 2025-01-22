28b270299ee0e26b0cb2c6da3e88928725a6cfd8
jefft0 <jeff@thefirst.org>
2025-01-22 18:27:27 +0100

    global: Update the copyright header for 2025.

--- graphics-items/aera-graphics-item-group.cpp
+++ graphics-items/aera-graphics-item-group.cpp
@@ -2,9 +2,9 @@
 //_/_/
 //_/_/ AERA Visualizer
 //_/_/ 
-//_/_/ Copyright (c) 2022-2023 Jeff Thompson
-//_/_/ Copyright (c) 2022-2023 Kristinn R. Thorisson
-//_/_/ Copyright (c) 2022-2023 Icelandic Institute for Intelligent Machines
+//_/_/ Copyright (c) 2022-2025 Jeff Thompson
+//_/_/ Copyright (c) 2022-2025 Kristinn R. Thorisson
+//_/_/ Copyright (c) 2022-2025 Icelandic Institute for Intelligent Machines
 //_/_/ http://www.iiim.is
 //_/_/
 //_/_/ --- Open-Source BSD License, with CADIA Clause v 1.0 ---
--- graphics-items/aera-visualizer-scene.cpp
+++ graphics-items/aera-visualizer-scene.cpp
@@ -2,9 +2,9 @@
 //_/_/
 //_/_/ AERA Visualizer
 //_/_/ 
-//_/_/ Copyright (c) 2018-2023 Jeff Thompson
-//_/_/ Copyright (c) 2018-2023 Kristinn R. Thorisson
-//_/_/ Copyright (c) 2018-2023 Icelandic Institute for Intelligent Machines
+//_/_/ Copyright (c) 2018-2025 Jeff Thompson
+//_/_/ Copyright (c) 2018-2025 Kristinn R. Thorisson
+//_/_/ Copyright (c) 2018-2025 Icelandic Institute for Intelligent Machines
 //_/_/ Copyright (c) 2021 Karl Asgeir Geirsson
 //_/_/ Copyright (c) 2021 Leonard Eberding
 //_/_/ http://www.iiim.is
